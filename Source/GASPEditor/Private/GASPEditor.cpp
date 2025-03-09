3b0afbbf770332387a45e5f9a79882d7d7e94f61
Anaylan <61459651+Anaylan@users.noreply.github.com>
2025-03-09 19:03:21 +0600

    add blend node, fix physics asset, e.t.c improvement

---
+++ Source/GASPEditor/Private/GASPEditor.cpp
@@ -0,0 +1,17 @@
+﻿#include "GASPEditor.h"
+
+#define LOCTEXT_NAMESPACE "FGASPEditorModule"
+
+void FGASPEditorModule::StartupModule()
+{
+    
+}
+
+void FGASPEditorModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FGASPEditorModule, GASPEditor)
---
+++ Source/GASPEditor/Public/Nodes/AnimGraphNode_GameplayTagsBlend.h
@@ -0,0 +1,36 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AnimGraphNode_BlendListBase.h"
+#include "AnimGraphNode_GameplayTagsBlend.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GASP_API UAnimGraphNode_GameplayTagsBlend : public UAnimGraphNode_BlendListBase
+{
+	GENERATED_BODY()
+
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Settings")
+	FAnimNode_GameplayTagsBlend Node;
+
+public:
+	UAnimGraphNode_GameplayTagsBlend();
+
+	virtual void PostEditChangeProperty(FPropertyChangedEvent& ChangedEvent) override;
+
+	virtual FText GetNodeTitle(ENodeTitleType::Type TitleType) const override;
+
+	virtual FText GetTooltipText() const override;
+
+	virtual void ReallocatePinsDuringReconstruction(TArray<UEdGraphPin*>& PreviousPins) override;
+
+	virtual void CustomizePinData(UEdGraphPin* Pin, FName SourcePropertyName, int32 PinIndex) const override;
+
+protected:
+	static void GetBlendPinProperties(const UEdGraphPin* Pin, bool& bBlendPosePin, bool& bBlendTimePin);
+};
